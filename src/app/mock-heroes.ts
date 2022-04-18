36dc2547cc625002942705a44f4b1a5d8c09351b
TERY <fire_jacky@mail.ru>
2022-04-18 16:32:29 +0300

    Project finished

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Spider-man' },
+  { id: 12, name: 'Superman' },
+  { id: 13, name: 'Aquaman' },
+  { id: 14, name: 'Green Lantern' },
+  { id: 15, name: 'Magneto' },
+  { id: 16, name: 'Professor X' },
+  { id: 17, name: 'Flash' },
+  { id: 18, name: 'Ironman'},
+  { id: 19, name: 'Wonder Woman' },
+  { id: 20, name: 'Black Widow' }
+];
